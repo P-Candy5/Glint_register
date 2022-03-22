a3596bff4f4b3f2351ebd82b6f922befe75d06f3
George, B. Smith <georgebsmith.tech@gmail.com>
2022-03-22 16:33:44 +0100

    fixed the harmburger menu and some css

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const burg = get(".burg");
 burg.style.cursor = "pointer";
 
 burg.addEventListener("click", function () {
-  get(".main-nav").classList.remove("slide-away");
+  get(".main-nav").classList.toggle("slide-away");
 });
-
 
 const sign = get(".sign");
 
-sign.addEventListener("click", function() {
-get("main-nav").classList.remove("slide-in")
-})
+sign.addEventListener("click", function () {
+  get(".main-nav").classList.add("slide-away");
+});
